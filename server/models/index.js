2bdeaf5c2644dc827323332504824896a01afd07
knatsuki92 <knatsuki92@gmail.com>
2015-02-27 20:28:45 -0800

    end of 2/27/15

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,17 @@
 var db = require('../db');
 
-
-
-
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (req, res) {
+
+      db.query('SELECT * FROM messages', [], function(err, results) {
+        if(err) throw err;
+        else {
+          res.status(200).end(results);
+        }
+
+      });
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
@@ -16,3 +22,4 @@ module.exports = {
   }
 };
 
+// db.quer
--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+-- CREATE TABLE messages (
   /* Describe your table here.*/
-);
+-- );
 
 /* Create other tables and define schemas for them here! */
 
@@ -14,4 +14,64 @@ CREATE TABLE messages (
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
+
+
+
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'users'
+--
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `message` VARCHAR(140) NULL DEFAULT NULL,
+  `roomname` VARCHAR(20) NULL DEFAULT NULL,
+  `user` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (user) REFERENCES `users` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `users` (`id`,`username`) VALUES
+-- ('','');
+-- INSERT INTO `messages` (`id`,`message`,`roomname`,`user`) VALUES
+-- ('','','','');
 
